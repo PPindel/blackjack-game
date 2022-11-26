54067e36a790fa8c9cd1ae3a412d2b3ef9fcd080
PPindel <przemyslaw.pindel@gmail.com>
2022-11-26 01:29:58 +0000

    Add feature - Ace card is changing its value from 11 to 1 when score is above 21

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,180 +1,265 @@
-/* global variables below 
+/* global variable below */
 
-let two = parseInt(2);
-let three = parseInt(3);
-let four = parseInt(4);
-let five = parseInt(5);
-let six = parseInt(6);
-let seven = parseInt(7);
-let eight = parseInt(8);
-let nine = parseInt(9);
-let ten = parseInt(10);
-let jack = parseInt(10);
-let queen = parseInt(10);
-let king = parseInt(10);
-let ace = parseInt(11);
-
-let deck = [two, two, two, two, three, three, three, three, four, four, four, four, five, five, five, five, six, six, six, six, seven, seven, seven, seven,
-    eight, eight, eight, eight, nine, nine, nine, nine, ten, ten, ten, ten, jack, jack, jack, jack, queen, queen, queen, queen, king, king, king, king, ace, ace, ace, ace
-]; */
-
-let fullDeck = [
-    {rank: "2",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(2)},
-    {rank: "3",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(3)},
-    {rank: "4",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(4)},
-    {rank: "5",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(5)},
-    {rank: "6",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(6)},
-    {rank: "7",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(7)},
-    {rank: "8",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(8)},
-    {rank: "9",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(9)},
-    {rank: "10",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(10)},
-    {rank: "J",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(10)},
-    {rank: "Q",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(10)},
-    {rank: "K",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(10)},
-    {rank: "A",
-    suite: `<i class="fa-solid fa-clover"></i>`,
-    value: parseInt(11)},
-    {rank: "2",
-    suite: `<i class="fa-solid fa-heart"></i></i>`,
-    value: parseInt(2)},
-    {rank: "3",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(3)},
-    {rank: "4",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(4)},
-    {rank: "5",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(5)},
-    {rank: "6",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(6)},
-    {rank: "7",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(7)},
-    {rank: "8",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(8)},
-    {rank: "9",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(9)},
-    {rank: "10",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(10)},
-    {rank: "J",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(10)},
-    {rank: "Q",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(10)},
-    {rank: "K",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(10)},
-    {rank: "A",
-    suite: `<i class="fa-solid fa-heart"></i>`,
-    value: parseInt(11)},
-    {rank: "2",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(2)},
-    {rank: "3",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(3)},
-    {rank: "4",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(4)},
-    {rank: "5",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(5)},
-    {rank: "6",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(6)},
-    {rank: "7",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(7)},
-    {rank: "8",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(8)},
-    {rank: "9",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(9)},
-    {rank: "10",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(10)},
-    {rank: "J",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(10)},
-    {rank: "Q",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(10)},
-    {rank: "K",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(10)},
-    {rank: "A",
-    suite: `<i class="fa-solid fa-diamond"></i>`,
-    value: parseInt(11)},
-    {rank: "2",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(2)},
-    {rank: "3",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(3)},
-    {rank: "4",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(4)},
-    {rank: "5",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(5)},
-    {rank: "6",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(6)},
-    {rank: "7",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(7)},
-    {rank: "8",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(8)},
-    {rank: "9",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(9)},
-    {rank: "10",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(10)},
-    {rank: "J",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(10)},
-    {rank: "Q",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(10)},
-    {rank: "K",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(10)},
-    {rank: "A",
-    suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
-    value: parseInt(11)}
+let fullDeck = [{
+        rank: "2",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(2)
+    },
+    {
+        rank: "3",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(3)
+    },
+    {
+        rank: "4",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(4)
+    },
+    {
+        rank: "5",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(5)
+    },
+    {
+        rank: "6",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(6)
+    },
+    {
+        rank: "7",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(7)
+    },
+    {
+        rank: "8",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(8)
+    },
+    {
+        rank: "9",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(9)
+    },
+    {
+        rank: "10",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "J",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "Q",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "K",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "A",
+        suite: `<i class="fa-solid fa-clover"></i>`,
+        value: parseInt(11)
+    },
+    {
+        rank: "2",
+        suite: `<i class="fa-solid fa-heart"></i></i>`,
+        value: parseInt(2)
+    },
+    {
+        rank: "3",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(3)
+    },
+    {
+        rank: "4",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(4)
+    },
+    {
+        rank: "5",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(5)
+    },
+    {
+        rank: "6",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(6)
+    },
+    {
+        rank: "7",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(7)
+    },
+    {
+        rank: "8",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(8)
+    },
+    {
+        rank: "9",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(9)
+    },
+    {
+        rank: "10",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "J",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "Q",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "K",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "A",
+        suite: `<i class="fa-solid fa-heart"></i>`,
+        value: parseInt(11)
+    },
+    {
+        rank: "2",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(2)
+    },
+    {
+        rank: "3",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(3)
+    },
+    {
+        rank: "4",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(4)
+    },
+    {
+        rank: "5",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(5)
+    },
+    {
+        rank: "6",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(6)
+    },
+    {
+        rank: "7",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(7)
+    },
+    {
+        rank: "8",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(8)
+    },
+    {
+        rank: "9",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(9)
+    },
+    {
+        rank: "10",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "J",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "Q",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "K",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "A",
+        suite: `<i class="fa-solid fa-diamond"></i>`,
+        value: parseInt(11)
+    },
+    {
+        rank: "2",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(2)
+    },
+    {
+        rank: "3",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(3)
+    },
+    {
+        rank: "4",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(4)
+    },
+    {
+        rank: "5",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(5)
+    },
+    {
+        rank: "6",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(6)
+    },
+    {
+        rank: "7",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(7)
+    },
+    {
+        rank: "8",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(8)
+    },
+    {
+        rank: "9",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(9)
+    },
+    {
+        rank: "10",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "J",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "Q",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "K",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(10)
+    },
+    {
+        rank: "A",
+        suite: `<i class="fa-brands fa-canadian-maple-leaf"></i>`,
+        value: parseInt(11)
+    }
 ]
 
 let i = 0;
@@ -187,32 +272,42 @@ function runGame() {
 
     hitButton.style.display = "inline";
     stayButton.style.display = "inline";
+    i = 0;
     shuffle();
-    userScore = 0;
-    cpuScore = 0;
+
+    if (fullDeck[51].rank === "A" && fullDeck[49].rank === "A") {
+        fullDeck[51].value = 1;
+    }
+
+    if ((fullDeck[50].value + fullDeck[48].value) > 21) {
+        if (fullDeck[50].rank === "A") {
+            fullDeck[50].value = 1;
+        } else if (fullDeck[48].rank === "A") {
+            fullDeck[48].value = 1;
+        }
+    }
 
     document.getElementById("user").innerHTML = `
-    <div class="cards">
-        <p class="topLeft">${fullDeck[51].rank}</p>
-        <p class="suite">${fullDeck[51].suite}</p>
-        <p class="bottomRight">${fullDeck[51].rank}</p>
-    </div>
-    <div class="cards">
-        <p class="topLeft">${fullDeck[49].rank}</p>
-        <p class="suite">${fullDeck[49].suite}</p>
-        <p class="bottomRight">${fullDeck[49].rank}</p>
-    </div>`;
+        <div class="cards">
+            <p class="topLeft">${fullDeck[51].rank}</p>
+            <p class="suite">${fullDeck[51].suite}</p>
+            <p class="bottomRight">${fullDeck[51].rank}</p>
+        </div>
+        <div class="cards">
+            <p class="topLeft">${fullDeck[49].rank}</p>
+            <p class="suite">${fullDeck[49].suite}</p>
+            <p class="bottomRight">${fullDeck[49].rank}</p>
+        </div>`;
     currentUserCards = [fullDeck[51].value, fullDeck[49].value];
 
     document.getElementById("cpu").innerHTML = `
-    <div class="cards" id="questionMark"><p class="suite"><i class="fa-regular fa-circle-question"></i></p></div>
-    <div class="cards">
-        <p class="topLeft">${fullDeck[48].rank}</p>
-        <p class="suite">${fullDeck[48].suite}</p>
-        <p class="bottomRight">${fullDeck[48].rank}</p>
-    </div>`;
+        <div class="cards" id="questionMark"><p id="questionMarkStyle" class="suite"><i class="fa-regular fa-circle-question"></i></p></div>
+        <div class="cards">
+            <p class="topLeft">${fullDeck[48].rank}</p>
+            <p class="suite">${fullDeck[48].suite}</p>
+            <p class="bottomRight">${fullDeck[48].rank}</p>
+        </div>`;
     currentCpuCards = [fullDeck[50].value, fullDeck[48].value];
-
 }
 
 function shuffle() {
@@ -257,20 +352,44 @@ function drawCards() {
     </div>`
     document.getElementById("user").innerHTML = userTable;
     i++;
+
+    let userScore = 0;
+    for (let x = 0; x < currentUserCards.length; x++) {
+        userScore += parseInt(currentUserCards[x]);
+        if (userScore > 21) {
+            for (let y = 0; y < currentUserCards.length; y++) {
+                if (currentUserCards[y] === 11) {
+                    currentUserCards[y] = 1;
+                }
+            }
+        }
+    }
 }
 
 function cpuDraw() {
     let cpuTable = document.getElementById("cpu").innerHTML;
-    let newCpuCard = fullDeck[i + 1].value;
+    let newCpuCard = fullDeck[i].value;
     currentCpuCards.push(newCpuCard);
     cpuTable += `
     <div class="cards">
-        <p class="topLeft">${fullDeck[i + 1].rank}</p>
-        <p class="suite">${fullDeck[i + 1].suite}</p>
-        <p class="bottomRight">${fullDeck[i + 1].rank}</p>
+        <p class="topLeft">${fullDeck[i].rank}</p>
+        <p class="suite">${fullDeck[i].suite}</p>
+        <p class="bottomRight">${fullDeck[i].rank}</p>
     </div>`
     document.getElementById("cpu").innerHTML = cpuTable;
     i++;
+
+    let cpuScore = 0;
+    for (let u = 0; u < currentCpuCards.length; u++) {
+        cpuScore += parseInt(currentCpuCards[u]);
+        if (cpuScore > 21) {
+            for (let v = 0; v < currentCpuCards.length; v++) {
+                if (currentCpuCards[v] === 11) {
+                    currentCpuCards[v] = 1;
+                }
+            }
+        }
+    }
 }
 
 function incrementUserPoints() {
@@ -288,16 +407,20 @@ function checkBust() {
     for (let e = 0; e < currentUserCards.length; e++) {
         userScore += parseInt(currentUserCards[e]);
         if (userScore > 21) {
-            document.getElementById("questionMark").innerHTML = `
-                <p class="topLeft">${fullDeck[50].rank}</p>
-                <p class="suite">${fullDeck[50].suite}</p>
-                <p class="bottomRight">${fullDeck[50].rank}</p>`;
-            alert("Bust!");
-            incrementCpuPoints();
-            hitButton.style.display = "none";
-            stayButton.style.display = "none";
+            bust();
         }
     }
+}
+
+function bust() {
+    alert("Bust!");
+    document.getElementById("questionMark").innerHTML = `
+        <p class="topLeft">${fullDeck[50].rank}</p>
+        <p class="suite">${fullDeck[50].suite}</p>
+        <p class="bottomRight">${fullDeck[50].rank}</p>`;
+    incrementCpuPoints();
+    hitButton.style.display = "none";
+    stayButton.style.display = "none";
 }
 
 function stayButtonAction() {
@@ -313,6 +436,18 @@ function stayButtonAction() {
     while (cpuScore < userScore) {
         cpuDraw();
         cpuScore += parseInt(currentCpuCards[(currentCpuCards.length - 1)]);
+        if (cpuScore > 21) {
+            for (let t = 0; t < currentCpuCards.length; t++) {
+                if (currentCpuCards[t] === 11) {
+                    currentCpuCards[t] = 1;
+                    cpuScore = 0;
+                    for (let r =0; r < currentCpuCards.length; r++) {
+                        cpuScore += parseInt(currentCpuCards[r]);
+                    }
+                }
+            }
+        }
+
     }
     checkResult();
     hitButton.style.display = "none";
